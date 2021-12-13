acd57fa8aaeb189a48b5d5a93136f11629464fca
johnysenpaii <johnjalosjos06@gmail.com>
2021-12-13 16:57:57 +0800

    new

--- js/main.js
+++ js/main.js
@@ -30,3 +30,14 @@ window.addEventListener('scroll', function(){
   let windowPosition = window.scrollY > 0;
   header.classList.toggle('scrolling-active', windowPosition);
 })
+
+var myLatLng = { lat: 38.3460, lng: -0.4907 };
+var mapOptions = {
+    center: myLatLng,
+    zoom: 7,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+
+};
+
+//create map
+var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
